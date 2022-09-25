d259591601546aa263e547d203de0f3864356f08
sangameswahr <112796162+sangameswahr@users.noreply.github.com>
2022-09-25 12:44:46 +0530

    format conversions

---
+++ C_Code/SESSION_031/format_conversion.c
@@ -0,0 +1,34 @@
+int main(void)
+{
+    int n = 36; 
+    char* addr_of_string = "36"; 
+
+
+}
+
+/* 
+    ALGORITHM 
+        itoa() 
+            INPUT : integer 
+            OUTPUT : string form of input integer 
+
+    ALGORITHM 
+        atoi() 
+            INPUT : a number in string form 
+            OUTPUT : an input number converted to integer form 
+*/ 
+
+
+/* 
+    A NUMBER IS AVAILABLE IN INTEGER FORM 
+    AIM IS TO DISPLAY AN INTEGER 
+
+    A NUMBER IN INTEGER FORM -> itoa() -> NUMBER IN STRING FORM -> puts() -> DISPLAY SCREEN 
+
+    A NUMBER IS AVAILABLE IN STRING FORM 
+    AIM IS TO PERFORM MATH OPERATION 
+    A NUMBER IN STRING FORM -> atoi() -> NUMBER IN INTEGER FORM -> STORE IN INTEGER VARIABLE 
+    PERFORM MATHEMATICAL OPERATIONS ON STORED VARIABLE 
+*/ 
+
+    
